6e18651c1001467e92519e57e807d506d34aac6c
Julio César  Gómez González <a01274966@itesm.mx>
2021-08-25 14:05:48 -0500

    Character selection complete

---
+++ BrackeysGJ2021/Assets/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject selectedskin;
+    public GameObject Player;
+
+    private Sprite playersprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playersprite = selectedskin.GetComponent<SpriteRenderer>().sprite;
+        Player.GetComponent<SpriteRenderer>().sprite = playersprite;
+    }
+
+}
---
+++ BrackeysGJ2021/Assets/SkinManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class SkinManager : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin = 0;
+    public GameObject playerSkin;
+
+    public void NextOption()
+    {
+        selectedSkin = selectedSkin + 1;
+        if(selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+        }
+        sr.sprite = skins[selectedSkin];
+        Debug.Log("Si pasa a la siguiente");
+    }
+    public void BackOption()
+    {
+        selectedSkin = selectedSkin - 1;
+        if (selectedSkin < 0)
+        {
+            selectedSkin = skins.Count - 1;
+        }
+        sr.sprite = skins[selectedSkin];
+        Debug.Log("Si pasa a la anterior");
+    }
+    public void PlayGame()
+    {
+        PrefabUtility.SaveAsPrefabAsset(playerSkin, "Assets/selectedskin.prefab");
+        SceneManager.LoadScene("Prueba");
+    }
+}
