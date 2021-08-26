c92fccf8d013b2f786e1dbb3db5aab471a237c94
Julio César  Gómez González <a01274966@itesm.mx>
2021-08-25 21:08:24 -0500

    Movement, Aim and Fire Joystick

--- BrackeysGJ2021/Assets/Scripts/Characters/Shooting.cs
+++ BrackeysGJ2021/Assets/Scripts/Characters/Shooting.cs
@@ -12,13 +12,20 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && !gameObject.GetComponent<PlayerManager>().isDead)
+        Aim();
+        if (Input.GetButton("Fire") && !gameObject.GetComponent<PlayerManager>().isDead)
         {
             Shoot();
         }
 
     }
 
+    void Aim()
+    {
+        float HorizontalAxis = Input.GetAxis("AimHorizontal");
+        float VerticalAxis = Input.GetAxis("AimVertical");
+        firePoint.transform.localEulerAngles = new Vector3(0f, 0f, Mathf.Atan2(HorizontalAxis, VerticalAxis) * -180 / Mathf.PI -90f);
+    }
     void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
